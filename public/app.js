f0a14c8f6d49dfdf79b517e82e996559af6f3cf6
Laure <laure.milian@gmail.com>
2016-11-04 11:35:59 +0100

    refactorisation partielle

--- public/app.js
+++ public/app.js
@@ -17,22 +17,21 @@
 
 		requestDone: function(response) {
 			console.log(response);
-			$('.companyName').html(response.companyName);
-			$('.catchPhrase').html(response.cathPhrase);
-			$('.productAdjective').html(response.productAdjective);
-			$('.productName').html(response.productName);
-			$('.productMaterial').html(response.productMaterial);
-			$('.color').html(response.color);
-			$('.price').html(response.price);
-			$('.url').html(response.url);
-			$('.businessImg').html('<img src="' + response.businessImg + '">');
-			$('.cardImg').html('<img src="' + response.cardImg +'">');
 			// Colonne de droite
 			$('.card').append('<h4>' + response.randomCard.name + '</h4>');
 			$('.card').append('<h4>' + response.randomCard.company.bs + '</h4>');
 			$('.card').append('<h4> Contact info </h4>');
 			$('.card').append('<h4>' + response.randomCard.address.streetB + '</h4>');
 			$('.card').append('<h4>' + response.randomCard.address.zipcode + " " + response.randomCard.address.city + '</h4>');
+			
+			// Colonne de gauche
+			for (var prop in response) {
+				$('.' + prop).html(response[prop]);
+				if (prop === 'businessImg' || prop === 'cardImg') {
+					$('.' + prop).html('<img src="' + response[prop] + '">');
+				};
+			}
+
 		},
 
 		requestFail: function() {
